be37cfe6adb4632979cef71d9a276c4fc711ea2f
VuKieuAnh <vukieuanh.hnue@gmail.com>
2025-03-25 14:36:08 +0700

    add oop

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-let products = ["Iphone 16 promax",
-    "VIVO X200 Pro Mini1",
-    "VIVO X200 Pro Mini2 ",
-    "VIVO X200 Pro Mini 3",
-    "Iphone 11 promax"];
+// ma, ten, so luong, gia
+let products = [
+    new Product("01", "But thien long", 3000, 100),
+    new Product("02", "But chi", 7000, 80),
+    new Product("03", "Bang hong ha ", 30000, 100)
+];
 // viet ham hien thi danh sach san pham
 function showAllProducts() {
     let content = "";
     for (let i = 0; i < products.length; i++) {
         content += `<tr>
-        <td>${products[i]}</td>
+        <td>${products[i].id}</td>
+        <td>${products[i].name}</td>
+        <td>${products[i].price}</td>
+        <td>${products[i].number}</td>
+        <td>${products[i].getAmount()}</td>
         <td><button>Edit</button></td>
         <td><button onclick="deleteByIndex(${i})">Delete</button></td>
     </tr>`
@@ -25,7 +30,11 @@ showAllProducts()
 // 2> Them vao mang
 // 3> Hien thi lai danh sach
 function addNewProduct() {
-    let newProduct = document.getElementById("name").value;
+    let name = document.getElementById("name").value;
+    let id = document.getElementById("id").value;
+    let price = +document.getElementById("price").value;
+    let number = +document.getElementById("number").value;
+    let newProduct = new Product(id,name, price,number);
     products.push(newProduct);
     showAllProducts();
 }
